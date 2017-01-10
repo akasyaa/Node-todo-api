0f1e2a1769d6ccf07096f9d6f7f5234110e432ba
Sang Choi <akasyashc@gmail.com>
2017-01-09 21:52:49 -0800

    REST API: POST/api

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const Todo = require('../models/todo');
 const User = require('../models/user');
@@ -6,18 +8,54 @@ const User = require('../models/user');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-const newTodo = new Todo({
-    text: 'proper todo'
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((e) => {
+            res.status(400).send(e);
+        })
 });
 
-const newUser = new User({
-    email: 'akasyashc@gmail.com'
+app.post('/api/users', (req, res) => {
+    const user = new User({
+        email: req.body.email
+    })
+
+    user.save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((e) => {
+            res.status(400).send(e);
+        })
 })
 
-newUser.save()
-    .then((doc) => {
-        console.log('Saved data', doc);
-    })
-    .catch((err) => {
-        console.log(JSON.stringify(err, undefined, 2));
-    });
+app.listen(3000, () => {
+    console.log('Started on port 3000.');
+})
+
+// const newTodo = new Todo({
+//     text: 'proper todo'
+// });
+//
+// const newUser = new User({
+//     email: 'akasyashc@gmail.com'
+// })
+//
+// newUser.save()
+//     .then((doc) => {
+//         console.log('Saved data', doc);
+//     })
+//     .catch((err) => {
+//         console.log(JSON.stringify(err, undefined, 2));
+//     });
