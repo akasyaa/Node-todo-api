0749d6452930c846efcd1269abcb1bcf1b0d03c3
Sang Choi <akasyashc@gmail.com>
2017-01-13 22:12:58 -0800

    Take env config out of server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,9 @@
+require('./config');
+
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./router');
-
-const env = process.env.NODE_ENV || 'development';
-
-console.log('env:', env);
-
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
